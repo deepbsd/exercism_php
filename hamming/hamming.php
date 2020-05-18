be87035356ea15e6a09c79db08b3d55463b895d8
David S. Jackson <deepbsd@yahoo.com>
2020-05-18 16:01:48 -0400

    moved line in hamming.php

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -2,11 +2,10 @@
 
 function distance(string $strandA, string $strandB) : int
 {
-    $count = 0;
     if ( strlen($strandA) != strlen($strandB) ) {
         throw new InvalidArgumentException("DNA strands must be of equal length.");
     }
-
+    $count = 0;
     for ($i=0; $i<strlen($strandA); $i++)
         if ( $strandA[$i] != $strandB[$i] ) $count+=1; 
 
